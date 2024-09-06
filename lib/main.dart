ea8b77ff4bed4d3a52d837ed65b214295b3a396a
ASUS <purnorabby0@gmail.com>
2024-09-07 00:41:21 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
 
 void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ShoppingCart(),
+    );
+  }
+}
 
 class ShoppingCart extends StatefulWidget {
   @override

4d619035b336aef8cf190e7b29166277f4d23b1f
ASUS <purnorabby0@gmail.com>
2024-09-07 00:28:19 +0600

    default apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,151 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ShoppingCart(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ShoppingCart extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ShoppingCartState createState() => _ShoppingCartState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ShoppingCartState extends State<ShoppingCart> {
+  // Item quantities
+  int pulloverQuantity = 1;
+  int tshirtQuantity = 1;
+  int sportDressQuantity = 1;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Item prices
+  final double pulloverPrice = 51.0;
+  final double tshirtPrice = 30.0;
+  final double sportDressPrice = 43.0;
+
+  // Calculate total
+  double get totalAmount {
+    return (pulloverQuantity * pulloverPrice) +
+        (tshirtQuantity * tshirtPrice) +
+        (sportDressQuantity * sportDressPrice);
+  }
+
+  // Snackbar message on checkout
+  void _showCheckoutMessage() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Congratulations! You have checked out successfully.')),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('My Bag'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: _build_product_sample(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _build_product_sample() {
+    return Column(
+        children: [
+          buildCartItem(
+              'Pullover',
+              'Black',
+              'L',
+              pulloverPrice,
+              pulloverQuantity,
+              'assets/images/pullover.png', // Add image asset path here
+                  (newValue) {
+                setState(() {
+                  pulloverQuantity = newValue;
+                });
+              }
+          ),
+          buildCartItem(
+              'T-Shirt',
+              'Gray',
+              'L',
+              tshirtPrice,
+              tshirtQuantity,
+              'assets/images/tshirt.png', // Add image asset path here
+                  (newValue) {
+                setState(() {
+                  tshirtQuantity = newValue;
+                });
+              }
+          ),
+          buildCartItem(
+              'Sport Dress',
+              'Black',
+              'M',
+              sportDressPrice,
+              sportDressQuantity,
+              'assets/images/flutter_logo.png', // Add image asset path here
+                  (newValue) {
+                setState(() {
+                  sportDressQuantity = newValue;
+                });
+              }
+          ),
+          const SizedBox(height: 20),
+          Text('Total amount: \$${totalAmount.toStringAsFixed(2)}'),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: 400,
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              onPressed: _showCheckoutMessage,
+              child: const Text('CHECK OUT',style: TextStyle(color: Colors.white,)),
+            ),
+          )
+        ],
+      );
+  }
+
+  // Helper to build each cart item with ListTile
+  Widget buildCartItem(String name, String color, String size, double price, int quantity, String imagePath, Function(int) onChanged) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      leading: Image.asset(
+        'assets/images/T-shirt.png',
+        width: 50, // Set the size for the logo
+        height: 50,
+        fit: BoxFit.cover, // Ensure the image covers the space properly
+      ),
+      title: Text(name),
+      subtitle: Text('Color: $color, Size: $size'),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            onPressed: () {
+              if (quantity > 1) {
+                onChanged(quantity - 1);
+              }
+            },
+            icon: const Icon(Icons.remove),
+          ),
+          Text(quantity.toString()),
+          IconButton(
+            onPressed: () {
+              onChanged(quantity + 1);
+            },
+            icon: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10),
+          Text('\$${price.toStringAsFixed(2)}'),
+        ],
+      ),
     );
   }
 }
